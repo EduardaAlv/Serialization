ec6066676257a24705321e70fd8b56a485729917
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2023-01-02 16:43:26 -0300

    Serialização de um objeto

---
+++ Serialization/Models/Objeto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization.Models
+{
+    public class Objeto
+    {
+
+        public Objeto(int id, string nome, decimal preco) {
+
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serialization.Models;
+using Newtonsoft.Json;
+
+Objeto objeto = new Objeto(1, "Objeto 1", 25.00M);
+
+
+string serializado = JsonConvert.SerializeObject(objeto, Formatting.Indented);
+
+Console.WriteLine(serializado);
