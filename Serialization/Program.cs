6f900be09cda29a8e8b27131c41f803d455fee22
EduardaAlv <eduardasantosalveseduarda@gmail.com>
2023-01-02 17:03:11 -0300

    Serialização de listas

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,9 +1,18 @@
 ﻿using Serialization.Models;
 using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+using System.Net.Http;
 
 Objeto objeto = new Objeto(1, "Objeto 1", 25.00M);
+Objeto objeto2 = new Objeto(1, "Objeto 2", 25.00M);
 
+List<Objeto> list = new List<Objeto>();
 
-string serializado = JsonConvert.SerializeObject(objeto, Formatting.Indented);
+list.Add(objeto);
+list.Add(objeto2);
 
-Console.WriteLine(serializado);
+string serializado = JsonConvert.SerializeObject(list, Formatting.Indented);
+
+File.WriteAllText(Path.GetFullPath(@"..\..\..\") + "\\Arquivos\\objetos.json", serializado);
+
+//Console.WriteLine(serializado);
